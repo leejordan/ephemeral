05e824b335e46eac3e1a19080354e9768c8e68c3
Lee <ldjordan@gmail.com>
2017-11-27 17:48:03 +0000

    aesthetic demo

---
+++ animate.js
@@ -0,0 +1,88 @@
+var word = document.getElementById('word');
+
+var noticeMe = document.getElementById('notice-me').onclick=function(){
+    anime({
+        targets: word.children[1],
+        color: [
+          { value: '#ccc', duration: 200, delay: 0, easing: 'easeInOutQuad' },
+          { value: '#3333ff', duration: 400, delay: 200, elasticity: 100, easing: 'easeInOutQuad' },
+          { value: '#000', duration: 3000, delay: 200, elasticity: 100, easing: 'easeInOutQuad' }
+        ],
+        translateY: [
+          { value: -35, duration: 400, elasticity: 100, delay: 0, easing: 'easeInOutQuad' },
+          { value: 0, duration: 200, delay: 200, elasticity: 100, easing: 'easeInOutQuad' }
+        ],
+        scale: [
+            { value: '1.05', duration: 100, delay: 400, easing: 'easeInOutQuad' },
+            { value: '1', duration: 30, delay: 0, easing: 'easeInOutQuad' },
+        ]
+    });
+};
+
+var goodbye = document.getElementById('goodbye').onclick=function(){
+    anime({
+        targets: word.children[1],
+        opacity: [
+            { value: '0.05', duration: 300, delay: 0, elasticity: 100, easing: 'easeInOutQuad' },
+            { value: '1', duration: 1000, delay: 3000, elasticity: 100, easing: 'easeInOutQuad' }
+        ],
+        scale: [
+            { value: '0.6', duration: 300, delay: 0, easing: 'easeInOutQuad' },
+            { value: '1', duration: 1000, delay: 3000, easing: 'easeInOutQuad' },
+        ]
+    });
+};
+
+var unhappy = document.getElementById('unhappy').onclick=function(){
+    anime({
+        targets: word.children[1],
+        color: [
+            { value: '#ccc', duration: 200, delay: 0, easing: 'easeInOutQuad' },
+            { value: '#eee', duration: 200, delay: 100, easing: 'easeInOutQuad' },
+            { value: '#ff9966', duration: 400, delay: 200, elasticity: 100, easing: 'easeInOutQuad' },
+            { value: '#000', duration: 3000, delay: 200, elasticity: 100, easing: 'easeInOutQuad' }
+        ],
+        rotate: [
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '-1', duration: 20, elasticity: 100 },
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '-1', duration: 20, elasticity: 100 },
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '-1', duration: 20, elasticity: 100 },
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '-1', duration: 20, elasticity: 100 },
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '-1', duration: 20, elasticity: 100 },
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '-1', duration: 20, elasticity: 100 },
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '-1', duration: 20, elasticity: 100 },
+            { value: '1', duration: 20, elasticity: 100 },
+            { value: '0', duration: 20, elasticity: 100 }
+        ],
+        translateY: [
+            { value: '-5%', duration: 1000, easing: 'easeOutElastic', elasticity: 500 },
+            { value: '0', duration: 300 }
+        ]
+    });
+};
+
+var happy = document.getElementById('happy').onclick=function(){
+    anime({
+        targets: word.children[1],
+        color: [
+            { value: '#ccc', duration: 200, delay: 0, easing: 'easeInOutQuad' },
+            { value: '#eee', duration: 200, delay: 100, easing: 'easeInOutQuad' },
+            { value: '#339933', duration: 400, delay: 200, elasticity: 100, easing: 'easeInOutQuad' },
+            { value: '#000', duration: 3000, delay: 200, elasticity: 100, easing: 'easeInOutQuad' }
+        ],
+        scale: [
+            { value: '1.1', duration: 400, delay: 0 },
+            { value: '1', duration: 400, delay: 600 },
+        ],
+        translateY: [
+            { value: '-5%', duration: 1000, easing: 'easeOutElastic', elasticity: 500 },
+            { value: '0', duration: 300 }
+        ]
+    });
+};
